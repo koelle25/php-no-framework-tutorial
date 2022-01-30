6cb2775c21b533007577dc9880f47fdc8ea30ae1
Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>
2022-01-30 16:32:38 +0100

    CH05 (Router) done
    Signed-off-by: Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>

---
+++ src/Routes.php
@@ -0,0 +1,21 @@
+<?php
+declare(strict_types=1);
+
+namespace NFT;
+
+return [
+    [
+        'GET',
+        '/hello-world',
+        function () {
+          echo 'Hello World';
+        },
+    ],
+    [
+        'GET',
+        '/another-route',
+        function () {
+          echo 'This works too';
+        },
+    ],
+];
