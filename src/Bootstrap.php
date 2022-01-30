295cf3f5697ef0bfd54de0a2e3601e28405dce12
Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>
2022-01-30 16:25:18 +0100

    CH04 (HTTP) done
    Signed-off-by: Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>

--- src/Bootstrap.php
+++ src/Bootstrap.php
@@ -22,4 +22,16 @@ if ($environment !== 'production') {
 }
 $whoops->register();
 
-throw new \Exception();
+/**
+ * Setup the request/response objects
+ */
+$request = new \Http\HttpRequest($_GET,$_POST, $_COOKIE, $_FILES, $_SERVER);
+$response = new \Http\HttpResponse();
+
+$response->setContent('<h1>Hello World</h1>');
+
+foreach ($response->getHeaders() as $header) {
+  header($header, false);
+}
+
+echo $response->getContent();
