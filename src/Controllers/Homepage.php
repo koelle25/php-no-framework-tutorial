48c23fae349e6fc4849952f5bf28ba04c52a4f27
Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>
2022-01-30 16:39:00 +0100

    CH07 (Inversion of Control) done
    Signed-off-by: Kevin Köllmann <kevin.koellmann001@stud.fh-dortmund.de>

--- src/Controllers/Homepage.php
+++ src/Controllers/Homepage.php
@@ -3,10 +3,19 @@ declare(strict_types=1);
 
 namespace NFT\Controllers;
 
+use Http\Response;
+
 class Homepage
 {
+  private $response;
+
+  public function __construct(Response $response)
+  {
+    $this->response = $response;
+  }
+
   public function show()
   {
-    echo '<h1>Homepage</h1>';
+    $this->response->setContent('<h1>Homepage</h1>');
   }
 }
